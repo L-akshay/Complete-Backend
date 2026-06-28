f071faeb76691ed0e2f54a615dc510ef505c530d
Lakshay Dawar <lakshaydawar2006@gmail.com>
2026-06-29 03:26:04 +0530

    Modify MongoDB connection in db.js
    Updated the MongoDB connection string to an empty string.

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 async function connectDB(){
-  await mongoose.connect("mongodb+srv://lakshaydawar2006_db_user:otxdtrEtIWsBtrh5@lakshays.qelom0s.mongodb.net/youtube")
+  await mongoose.connect("")
 
   console.log("connecte to DB")
 
